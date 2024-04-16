418eebbb6c5d8d1ba58465e00f60a86b582a9407
Voicu Librimir <voicu.librimir@gmail.com>
2024-04-16 21:58:27 +0300

    Add project files.

---
+++ Numartoare inversa pentru lansare.cpp
@@ -0,0 +1,16 @@
+﻿// Numartoare inversa pentru lansare.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main() {
+    // Numărătoarea inversă de la 10 la 1
+    for (int i = 10; i >= 1; --i) {
+        std::cout << i << std::endl;
+    }
+
+    // Afișează mesajul "Lansare!"
+    std::cout << "Lansare!" << std::endl;
+
+    return 0;
+}
